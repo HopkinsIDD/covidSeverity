3bddb33298d2dada879b8d3a02536e6e0496436f
salauer <stephenalauer@gmail.com>
2020-04-04 23:57:44 -0400

    got stan models working, but results aren't good

--- R/multilevel_age_prob.stan
+++ R/multilevel_age_prob.stan
@@ -1,12 +1,13 @@
 
 data {
-  int<lower=1> N;               // Number of subjects
+  int<lower=1> S;               // Number of subjects
   int<lower=1> J;               // the number of groups (Number of studies from which data comes)
   int<lower=1> A;               // max number of age groups (Number of studies from which data comes)
-  int<lower=1,upper=A> agegrp[N];   // vector of age group ids (indices)
-  int<lower=1,upper=J> id[N];   // vector of group ids (indices)
-  // matrix[N,A] X;                // fixed effects model integer array
-  int<lower=0,upper=1> Y[N];    // response variable - Outcome for each subect (died=1, survived=0)
+  int<lower=1,upper=A> agegrp[S];   // vector of age group ids (indices)
+  int<lower=1,upper=J> id[S];   // vector of group ids (indices)
+  // matrix[S,A] X;                // fixed effects model integer array
+  int<lower=1> N[S];
+  int<lower=0> Y[S];    // response variable - Outcome for each subect (died=1, survived=0)
 }
 
 // transformed data {
@@ -16,27 +17,25 @@ data {
 parameters {
   real beta0;
   vector[A] beta;               // Fixed effects
-  real<lower=0, upper=100> sigma;    // SDs for random effect
+  real<lower=0> sigma;    // SDs for random effect
   vector[J] beta_j;                  // Study Intercept (prob coefficient in each study)
 }
 
 model {
   // Priors on group parameters
-  beta0 ~ normal(0, 100);
+  // beta0 ~ normal(0, 100);
   beta ~ normal(0, 100);
 
   beta_j ~ normal(beta0, sigma);
 
   // The likelihood
-  Y ~ bernoulli_logit(beta_j[id] + beta[agegrp]);
+  Y ~ binomial_logit(N, beta_j[id] + beta[agegrp]);
 
 }
 
 generated quantities {         // simulate quantities of interest
-  real prob_mean;
   vector[A] prob_age;
 
-  prob_mean = inv_logit(beta0);
   prob_age = inv_logit(beta0 + beta);
 
 }
--- inst/first-pass-p-symp.R
+++ inst/first-pass-p-symp.R
@@ -7,69 +7,146 @@ library(googlesheets4)
 library(tidyverse)
 library(rstan)
 
-n_iter <- 1e3
-n_warmup <- 1e3-5e2
-p_adapt_delta <- 0.8
-n_max_treedepth <- 10
+n_iter <- 1e4
+n_warmup <- 1e4-5e3
+p_adapt_delta <- 0.99
+n_max_treedepth <- 20
 
 age_specific_data <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1pDfQ2SkO--F2WNfjZxx6t9V7K51IZW0bJKV5cwSCZfA/edit#gid=1769840547",
-                                               sheet="age risk")
+                                               sheet="age risk") %>%
+  filter(USE_pipeline==T)
 
 ## look at one conditional probability (e.g. proportion symptomatic pSymp_Inf)
-raw_symp_inf <- age_specific_data %>%
-  filter(USE_pipeline==T, param=="pSymp_Inf") %>%
+raw_params <- age_specific_data %>%
+  # filter(param=="pSymp_Inf") %>%
   mutate(N = ifelse(is.na(N),
-                    round(value*(1-value)*(qnorm(.975)/(valueR-value))^2),
+                    ifelse(!is.na(X), round(X/value),
+                           ifelse(!is.na(valueR),
+                                  round(value*(1-value)*(qnorm(.975)/(valueR-value))^2),
+                                  1000)),
                     N),
          X = ifelse(is.na(X),round(N*value),X))
 
 ## reallocate cases to new age groups
 ## first expand summarized data
-expanded_symp_inf <- c()
-## keep the studies separate
-studies <- unique(raw_symp_inf$source)
-num_studies <- length(studies)
-for(i in 1:num_studies){
-  tmp <- raw_symp_inf %>% filter(source==studies[i])
-  ## break out each age group
-  age_groups <- unique(tmp$ageL)
-  num_ag <- length(age_groups)
-  for(j in 1:num_ag){
-    tmp_ag <- tmp %>% filter(ageL==age_groups[j])
+expanded_dat <- c()
+## do each param separately
+params <- unique(raw_params$param)
+num_params <- length(params)
+for(i in 1:num_params){
+  param_dat <- filter(raw_params, param==params[i])
+  if(nrow(param_dat)==1){
+    next
+  }
+  ## keep the studies separate
+  studies <- unique(param_dat$source)
+  num_studies <- length(studies)
+  for(j in 1:num_studies){
+    tmp <- param_dat %>% filter(source==studies[j])
+    ## break out each age group
+    age_groups <- unique(tmp$ageL)
+    num_ag <- length(age_groups)
+    for(k in 1:num_ag){
+      tmp_ag <- tmp %>% filter(ageL==age_groups[k])
 
-    expanded_symp_inf <- bind_rows(expanded_symp_inf,
-                                   tibble(study=i,
-                                          param=tmp$param[1],
-                                          age=sample(tmp$ageL[j]:tmp$ageR[j],
-                                                     tmp$N[j], replace=T),
-                                          x=sample(c(rep(1,tmp$X[j]),
-                                                     rep(0,tmp$N[j]-tmp$X[j])),
-                                                   tmp$N[j], replace=F)))
+      expanded_dat <- bind_rows(expanded_dat,
+                                tibble(param=tmp_ag$param[1],
+                                       study=j,
+                                       age=sample(tmp_ag$ageL[1]:tmp_ag$ageR[1],
+                                                  tmp_ag$N[1], replace=T),
+                                       x=sample(c(rep(1,tmp_ag$X[1]),
+                                                  rep(0,tmp_ag$N[1]-tmp_ag$X[1])),
+                                                tmp_ag$N[1], replace=F)))
+    }
   }
 }
-
-age_symp_inf <- expanded_symp_inf %>%
+age_params <- expanded_dat %>%
   mutate(age_cat=cut(age,seq(0,100,by=10),right = FALSE),
-         age_grp=as.factor(age_cat) %>% as.numeric) #%>%
-  # group_by(study, age_cat) %>%
-  # summarize(X = sum(x),
-  #           N = n())
+         age_grp=as.factor(age_cat) %>% as.numeric) %>%
+  group_by(param, study, age_cat, age_grp) %>%
+  summarize(X=sum(x),
+            N=n())
 
 age_stan <- stan_model("R/multilevel_age_prob.stan")
 
 symp_inf_est <- sampling(age_stan,
-                         data=list(N=nrow(age_symp_inf),
-                                   J=max(age_symp_inf$study),
-                                   A=max(age_symp_inf$age_grp),
-                                   agegrp=age_symp_inf$age_grp,
-                                   id=age_symp_inf$study,
-                                   Y=age_symp_inf$x
-                         ),
+                         data=list(S=nrow(age_params %>%
+                                            filter(param=="pSymp_Inf")),
+                                   J=max(age_params$study[age_params$param=="pSymp_Inf"]),
+                                   A=max(age_params$age_grp),
+                                   agegrp=age_params$age_grp[age_params$param=="pSymp_Inf"],
+                                   id=age_params$study[age_params$param=="pSymp_Inf"],
+                                   N=age_params$N[age_params$param=="pSymp_Inf"],
+                                   Y=age_params$X[age_params$param=="pSymp_Inf"]),
+                         pars=c("prob_age"),
                          iter=n_iter,
                          warmup=n_warmup,
                          control=list(adapt_delta=p_adapt_delta,
                                       max_treedepth=n_max_treedepth),
                          save_warmup=F)
 
+death_symp_est <- sampling(age_stan,
+                           data=list(S=nrow(age_params %>%
+                                              filter(param=="pDeath_Symp")),
+                                     J=max(age_params$study[age_params$param=="pDeath_Symp"]),
+                                     A=max(age_params$age_grp),
+                                     agegrp=age_params$age_grp[age_params$param=="pDeath_Symp"],
+                                     id=age_params$study[age_params$param=="pDeath_Symp"],
+                                     N=age_params$N[age_params$param=="pDeath_Symp"],
+                                     Y=age_params$X[age_params$param=="pDeath_Symp"]),
+                           pars=c("prob_age"),
+                           iter=n_iter,
+                           warmup=n_warmup,
+                           control=list(adapt_delta=p_adapt_delta,
+                                        max_treedepth=n_max_treedepth),
+                           save_warmup=F)
+
+hosp_symp_est <- sampling(age_stan,
+                          data=list(S=nrow(age_params %>%
+                                             filter(param=="pHosp_Symp")),
+                                    J=max(age_params$study[age_params$param=="pHosp_Symp"]),
+                                    A=max(age_params$age_grp),
+                                    agegrp=age_params$age_grp[age_params$param=="pHosp_Symp"],
+                                    id=age_params$study[age_params$param=="pHosp_Symp"],
+                                    N=age_params$N[age_params$param=="pHosp_Symp"],
+                                    Y=age_params$X[age_params$param=="pHosp_Symp"]),
+                          pars=c("prob_age"),
+                          iter=n_iter,
+                          warmup=n_warmup,
+                          control=list(adapt_delta=p_adapt_delta,
+                                       max_treedepth=n_max_treedepth),
+                          save_warmup=F)
+
+icu_hosp_est <- sampling(age_stan,
+                          data=list(S=nrow(age_params %>%
+                                             filter(param=="pICU_Hosp")),
+                                    J=max(age_params$study[age_params$param=="pICU_Hosp"]),
+                                    A=max(age_params$age_grp),
+                                    agegrp=age_params$age_grp[age_params$param=="pICU_Hosp"],
+                                    id=age_params$study[age_params$param=="pICU_Hosp"],
+                                    N=age_params$N[age_params$param=="pICU_Hosp"],
+                                    Y=age_params$X[age_params$param=="pICU_Hosp"]),
+                          pars=c("prob_age"),
+                          iter=n_iter,
+                          warmup=n_warmup,
+                          control=list(adapt_delta=p_adapt_delta,
+                                       max_treedepth=n_max_treedepth),
+                          save_warmup=F)
+
 p_symp_inf_age <- extract(symp_inf_est, par="prob_age")[[1]]
-apply(p_symp_inf_age, 2, mean)
+apply(p_symp_inf_age, 2, median)
+
+p_death_symp_age <- extract(death_symp_est, par="prob_age")[[1]]
+apply(p_death_symp_age, 2, median)
+
+p_hosp_symp_age <- extract(hosp_symp_est, par="prob_age")[[1]]
+apply(p_hosp_symp_age, 2, median)
+
+p_icu_hosp_age <- extract(icu_hosp_est, par="prob_age")[[1]]
+apply(p_icu_hosp_age, 2, median)
+
+p_vent_symp <- rbinom(n=nrow(p_symp_inf_age),
+                      size=raw_params$N[raw_params$param=="pVent_Symp"],
+                      prob=raw_params$X[raw_params$param=="pVent_Symp"]/raw_params$N[raw_params$param=="pVent_Symp"])/
+  raw_params$N[raw_params$param=="pVent_Symp"]
+
