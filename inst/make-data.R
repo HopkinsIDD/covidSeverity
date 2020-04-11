b0aca40dbd6b5a76c4fc2dd35424effe268b5fe3
Kyra Grantz <kyragrantz@gmail.com>
2020-04-11 01:17:49 -0400

    adding DC, PR to list of regions for US runs

--- inst/make-data.R
+++ inst/make-data.R
@@ -3,7 +3,7 @@ library(covidSeverity)
 
 ## make geoid data
 USpop_geoid_agecat <- load_age_county_pop(census_api_key = "c235e1b5620232fab506af060c5f8580604d89c1",
-                                          modeled_states = state.abb, census_year = 2018) %>%
+                                          modeled_states = c(state.abb, "PR", "DC"), census_year = 2010) %>%
   format_age_county_pop() %>%
   make_age_matrix()
 
